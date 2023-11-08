46b0d352064af0d692c7efd5ca79943f0c0d2ccd
Rodrigo Carpio Cerda <rodri@kali.rcarpioc04@gmail.com>
2023-11-08 18:06:54 +0100

    toda la parte 1 acabada

--- ListLinked.h
+++ ListLinked.h
@@ -4,136 +4,152 @@
 
 template <typename T>
 class ListLinked : public List<T> {
-	private:
-		Node<T>* first;
-		int n;
 
-	public: 
-		void insert(int pos, T e){
-			if(pos < 0 || pos >n){
-				throw std::out_of_range ("fuera de rango1");
+    private:
+ 
+        Node<T>* first;
+	int n;
+
+    public:
+	
+	ListLinked(){
+		first = nullptr;
+		n = 0;
+	}
+
+	~ListLinked(){
+		Node<T>* aux  = first;
+		while(first != nullptr){
+			first = first->next;
+			delete aux;
+			aux = first;
+		}
+	}
+
+
+	T operator[](int pos){
+		if (pos >= 0 && pos < n){ 
+			Node<T>* aux = first;
+                        for(int i = 0 ; i != pos ; i++){
+                        	aux = aux->next;
+                        }
+                        return aux->data;
+		}
+		else { 
+			throw std::out_of_range("La posición no es válida.\n");
+	       	}
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &objeto){
+		Node<T>* aux =objeto.first;
+
+		while(aux != nullptr){
+			out << aux->data <<" "<< std::endl;
+			aux = aux->next;
+		};
+
+		return out;
+	}
+
+        void insert(int pos, T e){ 
+		if(pos < 0 || pos > n){
+			throw std::out_of_range("La posición no es válida.\n");
+		}
+		else {
+			Node<T>* aux = first;
+                        Node<T>* aux2 = nullptr;
+
+			for(int i = 0 ; i != pos ; i++){ 
+				aux2 = aux;
+				aux = aux-> next;
 			}
-			if(n == 0){
-				n++;
-				first = e;
-			
+
+			Node<T>* nuevo_node = new Node(e , aux );
+
+			if(pos == 0){
+				nuevo_node->next = first;
+				first = nuevo_node;
 			}
 			else{
-				T* aux=first;
-				T* prevaux=nullptr;
-				for(int i=0; i != pos; i++){
-					prevaux = aux;
-					aux= aux->next;
-				}
-				prevaux->next =e;
-				e->next = aux;
-				n++;
-
-			}
-		}
-
-		void append (T e){
-			T* aux = first;
-			for(int i=0; i<n; i++){
-				aux = aux->next;
-			}
-			aux-> next = e;
-			n++;
-		}
-
-		void prepend(T e){
-			e->next = first;
-			first = e;
-			n++;
-		}
-
-		T remove(int pos){
-			if (pos<0 || pos >n-1){
-				throw std::out_of_range("fuera de rango 2");
-			}
-			
-			delete get(pos);
-			n--;
-
-			
-		}
-
-		T get(int pos){
-			if(pos<0 || pos >n-1){
-				throw std::out_of_range("fuera de rango 3");
-			}
-			T* aux = first;
-			T* prevaux = nullptr;
-
-			for(int i=0; i<pos;i++){
-				prevaux=aux;
-				aux = aux->next;
-			}
-			return aux;
-		}
-
-		int search(T e){
-			T* aux = first;
-			int i=0;
-			for(i=0; i<n; i++){
-				if(aux == e){
-					return e;
-				}
-				else{
-					aux=aux->next;
-				}
-			}
-			if(i>=n){
-				return -1;
+				aux2->next = nuevo_node;
+				nuevo_node->next = aux;
 			}
 
+			n++;	
 		}
+	}
+	
+	void append(T e){
+		insert(n, e);
+	}
 
-		bool empty(){
-			if(n==0){
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-
-		int size(){
-			return n;
-		}
-
-		ListLinked(){
-			*first = nullptr;
-			n=0;
-		}
-
-		~ListLinked(){
-			T* aux=first;
-			T* prevaux=nullptr;
-			for(int i=0; i< n; i++){
-				prevaux = aux;
-				aux = aux->next;
-				delete prevaux;
-			}
-		}
-
-		T operator[] (int pos){
-		if (pos<0 || pos > n-1){
-			throw std::out_of_range("fuera de rango 4");
+	void prepend(T e){
+		insert(0, e);
+	}
+	
+	T remove(int pos){
+		if(pos < 0 || pos >=  n){
+			throw std::out_of_range("La posición no es válida.\n");
 		}
 		else{
-			T* aux = first;
-			for(int i=0; i!= pos; i++){
-				aux = aux -> next;
+			Node<T>* aux = first;
+                        Node<T>* aux2 = nullptr;
 
+                        for(int i = 0 ; i != pos ; i++){
+	                        aux2 = aux;
+                                aux = aux-> next;
+                        }
+			
+			T elemento = aux->data;
+			n--;
+
+			if(pos == 0){
+				first = aux->next;
+				return elemento;
 			}
-			return aux;
+			else{
+				aux2->next = aux->next;
+				delete aux;
+				return elemento;
+                	}	
 		}
+	}
+	
+	T get(int pos){
+		if(pos < 0 || pos >= n){
+			throw std::out_of_range("La posición no es válida.\n");
 		}
+		else{
+			Node<T>* aux = first;
+			for(int i = 0 ; i != pos ; i++){
+				aux = aux->next;
+			}
+			return aux->data;
+		}
+	}
+	
+	int search(T e){
+		Node<T>*  aux = first;
+                for(int i = 0 ; i < n ; i++){
+                        if(aux->data == e){
+				return i;
+			}
+			aux = aux->next;
+		}
+		return -1;
+	}
 
-		friend std::ostream&operator<<(std::ostream &out, const ListLinked<T> &list){
-			out << list << std::endl;
-			return out;
+	bool empty(){
+		if(n == 0){
+			return 1;
 		}
+		else{ 
+			return 0;
+		}
+	}
 
+	int size(){
+		return n;
+	}
 };
+
